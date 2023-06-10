a6e2ed8053e511caf56b914ed759035044455f4b
dheater <daniel@heatertech.com>
2023-06-09 23:41:06 -0500

    Show username@hostname all the time

--- p10k/.p10k.zsh
+++ p10k/.p10k.zsh
@@ -49,8 +49,8 @@
   # Left prompt segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    context                   # user@host
-    dir                       # current directory
+    context                   user@host
+    dir                       current directory
     vcs                       # git status
     command_execution_time    # previous command duration
     # =========================[ Line #2 ]=========================
@@ -108,7 +108,7 @@
   # Context format when not root: user@host. The whole thing grey.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE="%F{$grey}%n@%m%f"
   # Don't show context unless root or in SSH.
-  typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION=
+  # typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION=
 
   # Show previous command duration only if it's >= 5s.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=5
