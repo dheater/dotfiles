20946a1518f33d58ca5d5b96e4d4cd2a9b62cf6c
dheater <daniel@heater.dev>
2023-07-13 21:22:37 -0500

    Fix nvim colors

--- nvim/.config/nvim/lua/dheater/set.lua
+++ nvim/.config/nvim/lua/dheater/set.lua
@@ -24,6 +24,11 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-
 vim.opt.colorcolumn = "80"
 
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "ColorColumn", { bg="Black" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg="Black" })
+-- vim.api.nvim_set_hl(0, "Normal", { ctermfg=White,  ctermbg=Black })
