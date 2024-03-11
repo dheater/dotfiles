3381daba9bfb3ed5027a06450280aad2f4060ed3
dheater <daniel@heater.dev>
2024-03-11 11:52:37 -0500

    Install copilot.nvim

--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -12,31 +12,32 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use('nvim-telescope/telescope-packer.nvim')
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-  use 'mbbill/undotree'
+  use('mbbill/undotree')
+
+  use('github/copilot.vim')
 
   use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
       --- Uncomment the two plugins below if you want to manage the language servers from neovim
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
+      { 'neovim/nvim-lspconfig' },
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     }
   }
 
-  use {'mg979/vim-visual-multi', branch = 'master'}
-
+  use { 'mg979/vim-visual-multi', branch = 'master' }
 end)
